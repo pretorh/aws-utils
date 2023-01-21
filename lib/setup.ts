c4bfc72ad15abba6a1b61b7dfee47dfe3e2a575a
Hendri Pretorius <pretorh@gmail.com>
2023-01-21 11:36:44 +0200

    extract and export default aws config

--- lib/setup.ts
+++ lib/setup.ts
@@ -1,7 +1,8 @@
 import AWS from 'aws-sdk';
 
-AWS.config.update({
+export const defaultConfig = {
   region: process.env.AWS_REGION || 'eu-west-1',
-});
+};
 
+AWS.config.update(defaultConfig);
 export default AWS;
