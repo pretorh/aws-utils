af806591fe3467670c3ec0abab87b7181d673cb8
Hendri Pretorius <pretorh@gmail.com>
2023-02-04 13:45:59 +0200

    setup airbnb-typescript/base eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,30 @@ module.exports = {
     sourceType: 'module',
     ecmaVersion: 'latest',
   },
+  ignorePatterns: [
+    'dist/*',
+    'node_modules/*',
+  ],
+  overrides: [
+    {
+      files: '*.ts',
+      extends: [
+        'airbnb-base',
+        'airbnb-typescript/base',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+      plugins: [
+        '@typescript-eslint',
+      ],
+    },
+  ],
   rules: {
     'no-console': 'off', // cli
     'import/extensions': 'off', // module type
