6035076e73a4c63c76a1d821eed6bffb1a794e06
Hendri Pretorius <pretorh@gmail.com>
2023-02-11 09:50:02 +0200

    convert to typescript
    - rename files
    - add checks around possible null/undefined objects
    - add types to remove implicit any from parameters, return types
    - add null/undefined checks after adding explicit types
    - fix missing parameter in `dumpIncommingRules`
    - fail when no group id ressolved from name
    - split long line function declarations over multiple lines

--- bin/allow-ssh.ts
+++ bin/allow-ssh.ts
@@ -5,20 +5,24 @@ import * as ec2rules from '../lib/ec2-security-groups.js';
 const DESCRIPTION = 'allow incomming ssh from my ip';
 const DEFAULT_SSH_GROUP_NAME = process.env.SSH_GROUP_NAME || 'allow ssh';
 
-async function add(groupId) {
+async function add(groupId: string) {
   const ip = await publicIpv4();
   console.log('adding rule to allow %s', ip);
   await ec2rules.allowIncomming(groupId, 'tcp', `${ip}/32`, 22, DESCRIPTION);
 }
 
-async function removeOld(groupId) {
+async function removeOld(groupId: string) {
   const rules = await ec2rules.getIncommingRules(groupId);
 
-  const sshRules = rules.filter((rule) => rule.FromPort === 22 && rule.ToPort === 22);
+  const sshRules = (rules || []).filter((rule) => rule.FromPort === 22 && rule.ToPort === 22);
 
   const all = sshRules.map((rule) => {
-    const ips = rule.IpRanges.filter((range) => range.Description === DESCRIPTION);
+    const ipRanges = rule.IpRanges || [];
+    const ips = ipRanges.filter((range) => range.Description === DESCRIPTION);
     const removeRules = ips.map((ip) => {
+      if (!ip.CidrIp) {
+        return Promise.resolve();
+      }
       console.log('removing rule for %s', ip.CidrIp);
       return ec2rules.removeIncomming(groupId, 'tcp', ip.CidrIp, 22, DESCRIPTION);
     });
@@ -27,9 +31,12 @@ async function removeOld(groupId) {
   await Promise.all(all);
 }
 
-async function perform(action, groupId) {
+async function perform(action: string, groupId: string): Promise<void> {
   if (groupId === undefined) {
     const resolvedGroupId = await ec2rules.findGroupId(DEFAULT_SSH_GROUP_NAME);
+    if (!resolvedGroupId) {
+      throw new Error('Failed to resolve group name to id');
+    }
     console.log('resolved group "%s" to "%s"', DEFAULT_SSH_GROUP_NAME, resolvedGroupId);
     return perform(action, resolvedGroupId);
   }
@@ -47,5 +54,5 @@ async function perform(action, groupId) {
 
 perform(process.argv[2], process.argv[3])
   .then(() => process.exit(0))
-  .catch((err) => console.error(err.message))
+  .catch((err: Error) => console.error(err.message))
   .then(() => process.exit(1));
