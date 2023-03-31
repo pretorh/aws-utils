73f2fd3739911a10b465a08148d6d7c0d10907bc
Hendri Pretorius <pretorh@gmail.com>
2023-03-31 08:05:46 +0200

    upgrade: replace v2 S3 client with v3
    import/export from new client, create using default config, remove
    `.promise()` call

--- lib/setup.ts
+++ lib/setup.ts
@@ -1,5 +1,6 @@
 import AWS from 'aws-sdk';
 import { EC2 } from '@aws-sdk/client-ec2';
+import { S3 } from '@aws-sdk/client-s3';
 
 export const defaultConfig = {
   region: process.env.AWS_REGION || 'eu-west-1',
@@ -9,5 +10,5 @@ AWS.config.update(defaultConfig);
 
 export default {
   EC2,
-  S3: AWS.S3, // replace with v3 client
+  S3,
 };
