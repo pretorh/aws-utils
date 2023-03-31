c7add9c04826ed02abdf0775bfce1ebf1952e103
Hendri Pretorius <pretorh@gmail.com>
2023-03-31 08:08:24 +0200

    upgrade: remove old v2 aws-sdk

--- lib/setup.ts
+++ lib/setup.ts
@@ -1,12 +1,9 @@
-import AWS from 'aws-sdk';
 import { EC2 } from '@aws-sdk/client-ec2';
 import { S3 } from '@aws-sdk/client-s3';
 
 export const defaultConfig = {
   region: process.env.AWS_REGION || 'eu-west-1',
 };
-
-AWS.config.update(defaultConfig);
 
 export default {
   EC2,
